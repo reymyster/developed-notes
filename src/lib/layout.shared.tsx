36fb86ccf066a42476b846e930a9f7df4ebe626b
Rey Trajano <rey@reytrajano.com>
2025-08-26 21:47:12 -0700

    slightly more layout and content

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,4 +1,5 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { HouseIcon } from "lucide-react";
 
 /**
  * Shared layout configurations
@@ -12,15 +13,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+          <HouseIcon className="size-4" />
+          Developed Notes
         </>
       ),
     },
